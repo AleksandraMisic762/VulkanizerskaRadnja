72693f0fb90fdbb0c61c365e4be21811a45a59f6
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-03-20 18:34:20 +0100

    Test Radnja i VulkanizerskaRadnja i ispavljena klasa VulkanizerskaRadnja

---
+++ test/auto_radnja/VulkanizerskaRadnjaTest.java
@@ -0,0 +1,20 @@
+package auto_radnja;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import auto_radnja.VulkanizerskaRadnja;
+
+class VulkanizerskaRadnjaTest extends RadnjaTest{
+
+	@BeforeEach
+	void setUp() throws Exception {
+		radnja = new VulkanizerskaRadnja();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		radnja = null;
+	}
+
+}
